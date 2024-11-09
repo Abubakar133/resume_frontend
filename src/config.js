375a630a0b235efd3439af4867cd4bc704d4d3b4
Abubakar133 <f201023@cfd.nu.eddu.pk>
2024-11-10 02:49:48 +0500

    done

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 //export const BASE_URL = "https://insurance-snowy.vercel.app/api";
-export const BASE_URL = "http://127.0.0.1:5000/api";
+//export const BASE_URL = "http://127.0.0.1:5000/api";
 
  //export const BASE_URL = "https://resume-backend-puce.vercel.app/api";
 
- //export const BASE_URL = "https://resumebackend-production-f9bd.up.railway.app/api";
+ export const BASE_URL = "https://resumebackend-production-f9bd.up.railway.app/api";
 
  window.APP_CONFIG = {
     BASE_URL: BASE_URL
