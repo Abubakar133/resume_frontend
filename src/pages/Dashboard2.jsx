59f8e24ce986f5e01415336bf8f39b5d08c317d8
Abubakar133 <f201023@cfd.nu.eddu.pk>
2024-11-22 18:27:10 +0500

    add refreshed

--- src/pages/Dashboard2.jsx
+++ src/pages/Dashboard2.jsx
@@ -4,9 +4,16 @@ import Header from "../Dashboard_components/Header";
 import Footer from "../Dashboard_components/FOOTER";
 
 import PdfPreviewPage from "./pdf_preview";
-
+import React, { useEffect } from "react";
 
 const Dashboard2 = () => {
+  useEffect(() => {
+    
+    if (!window.location.hash.includes("#refreshed")) {
+      window.location.hash = "#refreshed"; 
+      window.location.reload(); // Refresh the page
+    }
+  }, []);
 return (
 <div className="flex flex-col min-h-screen w-full overflow-hidden">
   <Header />
